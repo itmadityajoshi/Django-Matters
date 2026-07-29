dbcab05261919ca9c7ae6a3e2e5689d85dede270
unknown <adityajoshi316@gmail.com>
2026-07-29 23:37:31 +0200

    serializer and deserializer  done

--- Drf/api/serializers.py
+++ Drf/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from .models import Person
 
 class PersonSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     age = serializers.IntegerField()
     city = serializers.CharField(max_length = 150) 
+
+
+    def create(self, validated_data):
+        return Person.objects.create(**validated_data)
--- Drf/api/views.py
+++ Drf/api/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 from .models import Person
 from .serializers import PersonSerializer
 from rest_framework.renderers import JSONRenderer
+import io
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+
 # Create your views here.
 
 def singleobj(req):
     data = Person.objects.get(id=1)
     serializer = PersonSerializer(data)
     # print(serializer.data)
-    json_data = JSONRenderer().render(serializer.data)
-        
-    return HttpResponse(json_data, content_type = 'application/json')
-   
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type = 'application/json')
 
+    return JsonResponse(serializer.data)
+   
+@csrf_exempt
 def multipleobj(req):
+    if req.method == "POST":
+        json = req.body
+        stream = io.BytesIO(json)
+        parsed_data = JSONParser().parse(stream)
+        serializer = PersonSerializer(data=parsed_data)  #here the data=arguments are dentoed as the serializer know, it wil deserialize the client requested raw data
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({"Created":"Successful"}, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+        # print(parsed_data)
+        # print(type(parsed_data))
     data = Person.objects.all()
     serializer = PersonSerializer(data, many=True)
-    json_data = JSONRenderer().render(serializer.data)
-    # print(json_data)
-    return HttpResponse(json_data, content_type = 'application/json')
+    return JsonResponse(serializer.data, safe=False)
